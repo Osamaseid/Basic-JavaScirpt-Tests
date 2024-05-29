1b3200b206c9074bd12c39d4ed5431615271bdab
osama seid <osaseid1@gmail.com>
2024-05-29 09:39:11 +0300

    this is javascript datatype

---
+++ Data-types/script.js
@@ -0,0 +1,45 @@
+// Numbers
+let age = 25;
+let temperature = 28.5;
+
+// Strings
+let name = "abdellah";
+let message = 'Welcome to JavaScript!';
+
+// Booleans
+let isLogged = true;
+let hasPermission = false;
+
+// Arrays
+let fruits = ["apple", "banana", "orange"];
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.length);
+
+// Objects
+let person = {
+  name: "mohammed",
+  age: 20,
+  email: "mohammed@example.com"
+};
+console.log(person);
+
+// Null
+let data = null;
+
+// Undefined
+let city;
+let keder;
+
+// Printing the values
+console.log(age);
+console.log(temperature);
+console.log(name);
+console.log(message);
+console.log(isLogged);
+console.log(hasPermission);
+console.log(fruits);
+console.log(numbers);
+console.log(person);
+console.log(data);
+console.log(city);
